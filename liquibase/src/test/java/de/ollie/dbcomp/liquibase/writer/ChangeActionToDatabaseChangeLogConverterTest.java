db8d10a195f990f177c439f3b8b18ff6291b2bbc
Oliver Lieshoff <o.lieshoff@schuetze-consulting.ag>
2020-12-18 13:09:48 +0100

    Made integrative tests more comfortable.

--- liquibase/src/test/java/de/ollie/dbcomp/liquibase/writer/ChangeActionToDatabaseChangeLogConverterTest.java
+++ liquibase/src/test/java/de/ollie/dbcomp/liquibase/writer/ChangeActionToDatabaseChangeLogConverterTest.java
@@ -3,11 +3,12 @@ package de.ollie.dbcomp.liquibase.writer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -17,15 +18,12 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import de.ollie.dbcomp.comparator.model.ChangeActionCRO;
+import de.ollie.dbcomp.comparator.model.actions.AddColumnChangeActionCRO;
 import de.ollie.dbcomp.comparator.model.actions.ColumnDataCRO;
 import de.ollie.dbcomp.comparator.model.actions.CreateTableChangeActionCRO;
 import de.ollie.dbcomp.comparator.model.actions.DropTableChangeActionCRO;
-import liquibase.change.Change;
-import liquibase.change.ColumnConfig;
-import liquibase.change.core.CreateTableChange;
-import liquibase.change.core.DropTableChange;
-import liquibase.changelog.ChangeSet;
 import liquibase.changelog.DatabaseChangeLog;
+import liquibase.serializer.core.xml.XMLChangeLogSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -37,6 +35,9 @@ public class ChangeActionToDatabaseChangeLogConverterTest {
 	private static final String SQL_TYPE_NAME = "sql type";
 	private static final String TABLE_NAME = "table";
 
+	private static final String XML_HEADER = "<?xml version=\"1.1\" encoding=\"UTF-8\" standalone=\"no\"?>\n" //
+			+ "<databaseChangeLog xmlns=\"http://www.liquibase.org/xml/ns/dbchangelog\" xmlns:ext=\"http://www.liquibase.org/xml/ns/dbchangelog-ext\" xmlns:pro=\"http://www.liquibase.org/xml/ns/pro\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.liquibase.org/xml/ns/dbchangelog-ext http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-ext.xsd http://www.liquibase.org/xml/ns/pro http://www.liquibase.org/xml/ns/pro/liquibase-pro-4.1.xsd http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-4.1.xsd\">\n";
+
 	@Accessors(chain = true)
 	@AllArgsConstructor
 	@Data
@@ -45,21 +46,16 @@ public class ChangeActionToDatabaseChangeLogConverterTest {
 		private String sqlType;
 	}
 
+	private static String databaseChangeLogToString(DatabaseChangeLog databaseChangeLog) throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(baos);
+		XMLChangeLogSerializer changeLogSerializer = new XMLChangeLogSerializer();
+		changeLogSerializer.write(databaseChangeLog.getChangeSets(), printStream);
+		return baos.toString();
+	}
+
 	@InjectMocks
 	private ChangeActionToDatabaseChangeLogConverter unitUnderTest;
-
-	private static void assertDatabaseChangeLogEquals(DatabaseChangeLog expected, DatabaseChangeLog returned,
-			Comparator<Change> comparator) {
-		assertEquals(expected, returned);
-		assertEquals(expected.getChangeSets().size(), returned.getChangeSets().size());
-		for (int i = 0, leni = expected.getChangeSets().size(); i < leni; i++) {
-			assertEquals(expected.getChangeSets().get(i), returned.getChangeSets().get(i));
-			for (int j = 0, lenj = expected.getChangeSets().get(i).getChanges().size(); j < lenj; j++) {
-				assertEquals(0, comparator.compare(expected.getChangeSets().get(i).getChanges().get(j),
-						returned.getChangeSets().get(i).getChanges().get(j)));
-			}
-		}
-	}
 
 	@DisplayName("Test of method 'convert(List<ChangeAction>)'.")
 	@Nested
@@ -73,104 +69,89 @@ public class ChangeActionToDatabaseChangeLogConverterTest {
 
 		@DisplayName("Returns an empty DatabaseChangeLog if an empty list is passed.")
 		@Test
-		void passAnEmptyList_ReturnsAEmptyDatabaseChangeLog() {
+		void passAnEmptyList_ReturnsAEmptyDatabaseChangeLog() throws Exception {
 			// Prepare
-			DatabaseChangeLog expected = new DatabaseChangeLog("change-log.xml");
+			String expected = databaseChangeLogToString(new DatabaseChangeLog("change-log.xml")).replace("\r", "");
 			// Run
-			DatabaseChangeLog returned = unitUnderTest.convert(new ArrayList<>());
+			String returned = databaseChangeLogToString(unitUnderTest.convert(new ArrayList<>())).replace("\r", "");
 			// Check
-			assertDatabaseChangeLogEquals(expected, returned, (c0, c1) -> 0);
+			assertEquals(expected, returned);
 		}
 
 		@DisplayName("Returns a DatabaseChangeLog with one ChangeSet and some CreateTableChanges if the passed list has "
 				+ "CreateTableChangeActionCRO's only.")
 		@Test
-		void passAListWithCreateTableChangeActionCROsOnly_ReturnsADatabaseChangeLogWithOneChangeSetAndAllPassedCreateTableChanges() {
+		void passAListWithCreateTableChangeActionCROsOnly_ReturnsADatabaseChangeLogWithOneChangeSetAndAllPassedCreateTableChanges()
+				throws Exception {
 			// Prepare
+			String expected = XML_HEADER //
+					+ "    <changeSet author=\"dm-comp\" id=\"ADD-CHANGE-SET-ID-HERE\" objectQuotingStrategy=\"LEGACY\" runOnChange=\"true\">\n" //
+					+ "        <createTable tableName=\"table1\">\n" //
+					+ "            <column name=\"column\" type=\"sql type\"/>\n" //
+					+ "        </createTable>\n" //
+					+ "        <createTable tableName=\"table2\"/>\n" //
+					+ "    </changeSet>\n" //
+					+ "</databaseChangeLog>\n";
 			List<ChangeActionCRO> actions = Arrays.asList( //
 					new CreateTableChangeActionCRO().setTableName(TABLE_NAME + 1) //
 							.addColumns(new ColumnDataCRO().setName(COLUMN_NAME).setSqlType(SQL_TYPE_NAME)), //
 					new CreateTableChangeActionCRO().setTableName(TABLE_NAME + 2) //
 			);
-			DatabaseChangeLog expected = new DatabaseChangeLog("change-log.xml"); //
-			ChangeSet changeSet = new ChangeSet("ADD-CHANGE-SET-ID-HERE", "dm-comp", false, true, null, null, null,
-					expected);
-			changeSet.addChange(
-					createCreateTableChange(TABLE_NAME + 1, null, new ColumnData(COLUMN_NAME, SQL_TYPE_NAME)));
-			changeSet.addChange(createCreateTableChange(TABLE_NAME + 2, null));
-			expected.addChangeSet(changeSet);
 			// Run
-			DatabaseChangeLog returned = unitUnderTest.convert(actions);
+			String returned = databaseChangeLogToString(unitUnderTest.convert(actions)).replace("\r", "");
 			// Check
-			assertDatabaseChangeLogEquals(expected, returned, (c0, c1) -> {
-				CreateTableChange ctc0 = (CreateTableChange) c0;
-				CreateTableChange ctc1 = (CreateTableChange) c1;
-				return Objects.compare(ctc0.getSchemaName(), ctc1.getSchemaName(), (o0, o1) -> o0.compareTo(o1))
-						+ Objects.compare(ctc0.getTableName(), ctc1.getTableName(), (o0, o1) -> o0.compareTo(o1))
-						+ compareColumnConfigs(ctc0.getColumns(), ctc1.getColumns());
-			});
-		}
-
-		private CreateTableChange createCreateTableChange(String tableName, String schemaName, ColumnData... columns) {
-			CreateTableChange change = new CreateTableChange();
-			change.setSchemaName(schemaName);
-			change.setTableName(tableName);
-			for (ColumnData column : columns) {
-				ColumnConfig columnConfig = new ColumnConfig();
-				columnConfig.setName(column.getName());
-				columnConfig.setType(column.getSqlType());
-				change.addColumn(columnConfig);
-			}
-			return change;
-		}
-
-		private int compareColumnConfigs(List<ColumnConfig> columns0, List<ColumnConfig> columns1) {
-			if (columns0.size() != columns1.size()) {
-				return 1;
-			}
-			for (int i = 0, leni = columns0.size(); i < leni; i++) {
-				ColumnConfig cc0 = columns0.get(i);
-				ColumnConfig cc1 = columns1.get(i);
-				if (!cc0.getName().equals(cc1.getName()) //
-						|| !cc0.getType().equals(cc1.getType()) //
-				) {
-					return 1;
-				}
-			}
-			return 0;
+			assertEquals(expected, returned);
 		}
 
 		@DisplayName("Returns a DatabaseChangeLog with one ChangeSet and some DropTableChanges if the passed list has "
 				+ "DropTableChangeActionCRO's only.")
 		@Test
-		void passAListWithDropTableChangeActionCROsOnly_ReturnsADatabaseChangeLogWithOneChangeSetAndAllPassedDropTableChanges() {
+		void passAListWithDropTableChangeActionCROsOnly_ReturnsADatabaseChangeLogWithOneChangeSetAndAllPassedDropTableChanges()
+				throws Exception {
 			// Prepare
+			String expected = XML_HEADER //
+					+ "    <changeSet author=\"dm-comp\" id=\"ADD-CHANGE-SET-ID-HERE\" objectQuotingStrategy=\"LEGACY\" runOnChange=\"true\">\n" //
+					+ "        <dropTable tableName=\"table1\"/>\n" //
+					+ "        <dropTable tableName=\"table2\"/>\n" //
+					+ "    </changeSet>\n" //
+					+ "</databaseChangeLog>\n";
 			List<ChangeActionCRO> actions = Arrays.asList( //
 					new DropTableChangeActionCRO().setTableName(TABLE_NAME + 1), //
 					new DropTableChangeActionCRO().setTableName(TABLE_NAME + 2) //
 			);
-			DatabaseChangeLog expected = new DatabaseChangeLog("change-log.xml"); //
-			ChangeSet changeSet = new ChangeSet("ADD-CHANGE-SET-ID-HERE", "dm-comp", false, true, null, null, null,
-					expected);
-			changeSet.addChange(createDropTableChange(TABLE_NAME + 1, null));
-			changeSet.addChange(createDropTableChange(TABLE_NAME + 2, null));
-			expected.addChangeSet(changeSet);
 			// Run
-			DatabaseChangeLog returned = unitUnderTest.convert(actions);
+			String returned = databaseChangeLogToString(unitUnderTest.convert(actions)).replace("\r", "");
 			// Check
-			assertDatabaseChangeLogEquals(expected, returned, (c0, c1) -> {
-				DropTableChange dtc0 = (DropTableChange) c0;
-				DropTableChange dtc1 = (DropTableChange) c1;
-				return Objects.compare(dtc0.getSchemaName(), dtc1.getSchemaName(), (o0, o1) -> o0.compareTo(o1))
-						+ Objects.compare(dtc0.getTableName(), dtc1.getTableName(), (o0, o1) -> o0.compareTo(o1));
-			});
+			assertEquals(expected, returned);
 		}
 
-		private DropTableChange createDropTableChange(String tableName, String schemaName) {
-			DropTableChange change = new DropTableChange();
-			change.setSchemaName(schemaName);
-			change.setTableName(tableName);
-			return change;
+		@DisplayName("Returns a DatabaseChangeLog with two ChangeSets and some CreateTableChanges and AddColumnChanges.")
+		@Test
+		void passAListWithAnAddColumnChange_ReturnsADatabaseChangeLogsWithTheCorrectChanges() throws Exception {
+			// Prepare
+			String expected = XML_HEADER //
+					+ "    <changeSet author=\"dm-comp\" id=\"ADD-CHANGE-SET-ID-HERE\" objectQuotingStrategy=\"LEGACY\" runOnChange=\"true\">\n" //
+					+ "        <createTable tableName=\"table1\">\n" //
+					+ "            <column name=\"column\" type=\"sql type\"/>\n" //
+					+ "        </createTable>\n" //
+					+ "        <addColumn tableName=\"table2\">\n" //
+					+ "            <column name=\"column1\" type=\"BIGINT\"/>\n" //
+					+ "        </addColumn>\n" //
+					+ "    </changeSet>\n" //
+					+ "</databaseChangeLog>\n";
+			List<ChangeActionCRO> actions = Arrays.asList( //
+					new CreateTableChangeActionCRO() //
+							.setTableName(TABLE_NAME + 1) //
+							.addColumns(new ColumnDataCRO().setName(COLUMN_NAME).setSqlType(SQL_TYPE_NAME)), //
+					new AddColumnChangeActionCRO() //
+							.setTableName(TABLE_NAME + 2) //
+							.setColumnName(COLUMN_NAME + 1) //
+							.setSqlType("BIGINT") //
+			);
+			// Run
+			String returned = databaseChangeLogToString(unitUnderTest.convert(actions)).replace("\r", "");
+			// Check
+			assertEquals(expected, returned);
 		}
 
 	}
